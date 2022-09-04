9ecc12a32f12a75714587b5042ac8464e0b9a2d8
Tigran <tigran2589@gmail.com>
2022-09-04 21:45:21 +0300

    configured charinfo and charlist components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,3 +1,5 @@
+import { Component } from "react"
+
 import AppHeader from "../AppHeader"
 import RandomChar from "../RandomChar"
 import CharList from "../CharList"
@@ -5,20 +7,33 @@ import CharInfo from "../CharInfo"
 
 import decoration from '../../resources/img/vision.png'
 
-const App = () => {
-    return (
-        <div className="app">
-            <AppHeader />
-            <main>
-                <RandomChar />
-                <div className="char__content">
-                    <CharList />
-                    <CharInfo />
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision" />
-            </main>
-        </div>
-    )
+class App extends Component {
+
+    state = {
+        selectedChar: null
+    }
+
+    onCharSelected = (id) => {
+        this.setState({
+            selectedChar: id
+        })
+    }
+
+    render() {
+        return (
+            <div className="app">
+                <AppHeader />
+                <main>
+                    <RandomChar />
+                    <div className="char__content">
+                        <CharList onCharSelected={this.onCharSelected} />
+                        <CharInfo charId={this.state.selectedChar} />
+                    </div>
+                    <img className="bg-decoration" src={decoration} alt="vision" />
+                </main>
+            </div>
+        )
+    }
 }
 
 export default App
--- src/components/CharList/CharList.js
+++ src/components/CharList/CharList.js
@@ -9,6 +9,10 @@ import Spinner from '../Spinner'
 
 class CharList extends Component {
 
+    constructor(props) {
+        super(props)
+    }
+
     state = {
         charList: [],
         loading: true,
@@ -47,7 +51,9 @@ class CharList extends Component {
             return (
                 <li
                     className="char__item"
-                    key={item.id}>
+                    key={item.id}
+                    onClick={() => this.props.onCharSelected(item.id)}
+                >
                     <img src={item.thumbnail} alt={item.name} style={imgStyle} />
                     <div className="char__name">{item.name}</div>
                 </li>
